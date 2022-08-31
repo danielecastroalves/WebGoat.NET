d83c9e192b43a2f2d83ea25e88bfdbc799fce158
Marianne Kéllen Albuquerque da Silva <marianne.silva@ambevtech.com.br>
2022-08-31 15:03:19 -0300

    Fixing xpath vulnerability

--- WebGoat/Content/XPathInjection.aspx.cs
+++ WebGoat/Content/XPathInjection.aspx.cs
@@ -1,11 +1,6 @@
+using Microsoft.Security.Application;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Xml;
-using System.Xml.XPath;
 
 namespace OWASP.WebGoat.NET
 {
@@ -15,9 +10,11 @@ namespace OWASP.WebGoat.NET
         private string xml = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><sales><salesperson><name>David Palmer</name><city>Portland</city><state>or</state><ssn>123-45-6789</ssn></salesperson><salesperson><name>Jimmy Jones</name><city>San Diego</city><state>ca</state><ssn>555-45-6789</ssn></salesperson><salesperson><name>Tom Anderson</name><city>New York</city><state>ny</state><ssn>444-45-6789</ssn></salesperson><salesperson><name>Billy Moses</name><city>Houston</city><state>tx</state><ssn>333-45-6789</ssn></salesperson></sales>";
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Request.QueryString["state"] != null)
+            string state = Request.QueryString["state"];
+
+            if (state != null)
             {
-                FindSalesPerson(Request.QueryString["state"]);
+                FindSalesPerson(Encoder.HtmlEncode(state));
             }
         }
 
@@ -25,7 +22,7 @@ namespace OWASP.WebGoat.NET
         {
             XmlDocument xDoc = new XmlDocument();
             xDoc.LoadXml(xml);
-            XmlNodeList list = xDoc.SelectNodes("//salesperson[state='" + state + "']");
+            XmlNodeList list = xDoc.SelectNodes(Encoder.XmlEncode("//salesperson[state='" + state + "']"));
             if (list.Count > 0)
             {
 
